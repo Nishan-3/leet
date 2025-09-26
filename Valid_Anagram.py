7bd779e4854209e374887cca55f34e342c34a0db
Nishan-3 <121813216+Nishan-3@users.noreply.github.com>
2025-09-26 08:42:49 -0500

    Update Valid_Anagram.py

--- Valid_Anagram.py
+++ Valid_Anagram.py
@@ -1,1 +1,34 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
+        """
+        here we can sort but the time and space compexity is n(log(n))
+        we want to find in 0(n) 
+
+        so here we wont sort but we can sort this . 
+         
+         here we have atmost 26 arrays . 
+
+         this can be solved wiht hasmap. 
+         mapping the character of count 
+
+         ord() is ascii value 
+         here lets take an example the ascii value of a is 80 
+         and ascii value of b   is 81 
+
+         so ord(b) - ord(a)  = 1
+
+         here new knowledge to learn so here in  python  we can't use list as the key in dict because its mutable 
+        """
+
+        res  = defaultdict(list) #Mapping the charCount toi list of anagrams
+
+        for s in strs:
+            count = [0]*26 
+
+            for c in s :
+                count[ord(c)-ord("s")] +=1
+            res[tuple(count)].append(s)
+        
+        returnlist(res.values())
+        
