6aae4d159f985d38fee6428ea6b5e0e331285599
Nishan-3 <121813216+Nishan-3@users.noreply.github.com>
2025-11-13 18:27:50 -0600

    Add binary tree class and traversal methods
    Implemented a binary tree class with preorder, inorder, and postorder traversal methods.

---
+++ bianary_tree/dfs_logic.py
@@ -0,0 +1,61 @@
+#here we will make  bianary tree  blueprint 
+
+class Btree:
+    def __init__(self,data):
+        self.data = data
+        self.left = None 
+        self.right = None 
+        
+    def __str__(self):
+        return str(self.data)
+        
+firstnode = Btree(1)
+secondnode =Btree(2)
+thirdnode = Btree(3)
+fouthnode = Btree(4)
+fifthnode = Btree(5)
+
+firstnode.left = secondnode
+firstnode.right = thirdnode
+secondnode.left = fouthnode
+secondnode.right =fifthnode
+
+
+
+#preorder dfs 
+print('pre order')
+def preorder(node):
+    if node == None:
+        return 
+    print(node.data)
+    preorder(node.left)
+    preorder(node.right)
+    
+preorder(firstnode)
+
+
+#Inorder dfs
+print('inorder dfs')
+def inorder(node):
+    if node ==None:
+        return 
+    inorder(node.left)
+    print(node.data)
+    inorder(node.right)
+
+preorder(firstnode)
+print('Post order')
+def postorder(node):
+    if node == None :
+        return 
+    postorder(node.left)
+    postorder(node.right)
+    print(node)
+    
+postorder(firstnode)
+
+    
+    
+
+    
+    
